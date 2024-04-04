ebf0f6a25d4b1d196deafaf2d8954bc814e4b0a5
arkiver <arkiver@protonmail.com>
2024-04-04 16:17:10 +0200

    Version 20240404.01. Add checks for bad config for unreachable .onion sites. Check IP .onion is 'resolved' to.

--- Dockerfile
+++ Dockerfile
@@ -3,14 +3,14 @@ RUN DEBIAN_FRONTEND=noninteractive DEBIAN_PRIORITY=critical apt-get -qqy -o Dpkg
   && DEBIAN_FRONTEND=noninteractive DEBIAN_PRIORITY=critical apt-get -qqy -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -o Dpkg::Options::=--force-unsafe-io install tor \
   && rm -rf /var/lib/apt/lists/*
 COPY . /grab
-RUN grep "VERSION = '20240327.08'" pipeline.py
+RUN grep "VERSION = '20240401.01'" pipeline.py
 RUN sed -i "s|TRACKER_ID = 'urls'|TRACKER_ID = 'urls-tor'|" pipeline.py \
-  && sed -i -E "s|VERSION = '[0-9]+\.[0-9]+'|VERSION = '20240327.10'|" pipeline.py \
+  && sed -i -E "s|VERSION = '[0-9]+\.[0-9]+'|VERSION = '20240404.01'|" pipeline.py \
   && sed -i -E "s|WGET_AT_COMMAND = \[([^]]+)\]|WGET_AT_COMMAND = \['torify', \1\]|" pipeline.py \
   && sed -i "s|title = 'URLs'|title = 'URLs on Tor'|" pipeline.py \
   && sed -i -E 's|<a href="[^"]+">Leaderboard</a>|<a href="https://tracker\.archiveteam\.org/urls-tor/">Leaderboard</a>|' pipeline.py \
   && sed -i -E "s|USE_DNS_SECURITY = [A-Za-z0-9]+|USE_DNS_SECURITY = False|" pipeline.py \
-  && sed -i "s|CheckIP(),|StartTor(),CheckIP(),|" pipeline.py \
+  && sed -i "s|CheckIP(),|StartTor(),CheckIP(),CheckForBadConfig(),|" pipeline.py \
   && sed -n -i -e "/class CheckRequirements/r pipeline_additional.py" -e 1x -e '2,${x;p}' -e '${x;p}' pipeline.py \
   && sed -i -E "s|'timeout': [0-9]+,|'timeout': 120,|" pipeline.py \
   && sed -i -E "s|'--timeout', '[0-9]+',|'--timeout', '120',|" pipeline.py \
--- pipeline_additional.py
+++ pipeline_additional.py
@@ -2,7 +2,7 @@
 class StartTor(SimpleTask):
     def __init__(self):
         SimpleTask.__init__(self, 'ConnectTor')
-        
+
     def process(self, item):
         command = ['service', 'tor']
         kwargs = {
@@ -19,34 +19,38 @@ class StartTor(SimpleTask):
 
 
 class CheckIP(SimpleTask):
+    COMMAND = WGET_AT_COMMAND + [
+        '--output-document', '-',
+        '--max-redirect', '0',
+        '--save-headers',
+        '--no-check-certificate',
+        '--no-hsts'
+    ]
+    KWARGS = {
+        'timeout': 60,
+        'capture_output': True
+    }
+
     def __init__(self):
         SimpleTask.__init__(self, 'CheckIP')
         self._counter = 0
-        
+
     def process(self, item):
         if self._counter <= 0:
-            command = WGET_AT_COMMAND + [
-                '--output-document', '-',
-                '--max-redirect', '0',
-                '--save-headers',
-                '--no-check-certificate',
-                '--no-hsts'
-            ]
-            kwargs = {
-                'timeout': 60,
-                'capture_output': True
-            }
+            command = CheckIP.COMMAND
+            kwargs = CheckIP.KWARGS
 
             url = 'https://check.torproject.org/'
             returned = subprocess.run(command+[url], **kwargs)
             assert returned.returncode == 0, 'Invalid return code {} on {}.'.format(returned.returncode, url)
             assert b'Congratulations. This browser is configured to use Tor.' in returned.stdout, 'Tor is not being used.'
-            
+
             url = 'https://duckduckgogg42xjoc72x3sjasowoarfbgcmvfimaftt6twagswzczad.onion/'
             returned = subprocess.run(command+[url], **kwargs)
             assert returned.returncode == 0, 'Invalid return code {} on {}.'.format(returned.returncode, url)
             assert b'<title>DuckDuckGo</title>' in returned.stdout,  'Cannot access DuckDuckGo onion site.'
-            
+            assert b'... 127.42.42.' in returned.stderr, 'Resolved .onion to wrong IP.'
+
             url = 'http://legacy-api.arpa.li/now'
             returned = subprocess.run(
                 command+[url],
@@ -78,4 +82,28 @@ class CheckIP(SimpleTask):
         else:
             self._counter -= 1
 
+
+class CheckForBadConfig(SimpleTask):
+    def __init__(self):
+        SimpleTask.__init__(self, 'CheckForBadConfig')
+
+    def process(self, item):
+        for url in (
+            'http://3g2upl4pq6kufc4m.onion/',
+            'https://3g2upl4pq6kufc4m.onion/',
+            'http://puckduckgogg42xjoc72x3sjasowoarfbgcmvfimaftt6twagswzczad.onion/',
+            'https://puckduckgogg42xjoc72x3sjasowoarfbgcmvfimaftt6twagswzczad.onion/'
+        ):
+            returned = subprocess.run(
+                CheckIP.COMMAND+[url],
+                **CheckIP.KWARGS
+            )
+            assert len(returned.stdout) == 0, 'Bad stdout on {}, got {}.'.format(url, repr(returned.stdout))
+            assert re.search(
+                b'ERROR torsocks\\[([0-9]+)\\]: General SOCKS server failure \\(in socks5_recv_connect_reply\\(\\) at socks5\\.c:527\\)\n'
+                b'failed: Connection refused\\.\n$',
+                returned.stderr
+            ), 'Bad stderr on {}, got {}.'.format(url, repr(returned.stderr))
+            assert returned.returncode == 4, 'Invalid return code {} on {}.'.format(returned.returncode, url)
+            assert b'... 127.42.42.' in returned.stderr, 'Resolved .onion to wrong IP.'
 
